7aaae13844e2ad621c2290de46036a25f8f0cd9e
Abhishek Kumar Tiwari <aktiwari158@gmail.com>
2020-04-15 22:00:24 +0530

    Remove the file-loader, using url-loader instead

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: 'url-loader',
           },
         ],
       }
